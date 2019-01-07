b868a16f53c1d474d62f654ace021536d3d00a2d
Borui Tao <borui.tao@mail.mcgill.ca>
2019-01-07 13:42:34 -0500

    add more scenes

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,42 +1,62 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 1f;
+    [SerializeField] AudioClip mainEngine;
 
     Rigidbody rigitBody;
     AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+
+    // Use this for initialization
+    void Start () {
         rigitBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update () {
-        Thrust(); 
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)
+        {
+            return;
+        }
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f);
                 break;
             default:
-                print("Dead");
+                state = State.Dying;
+                Invoke("LoadFirstScene", 1f);
                 break;
         }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoaFirstScene()
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void Thrust()
